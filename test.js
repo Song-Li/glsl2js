d3564fb699183f24600cf243b1b80e62653ec020
Song-Li <lisong1013@gmail.com>
2018-04-10 21:10:56 +0000

    first

---
+++ test.js
@@ -0,0 +1,22 @@
+var GLSL = require('./');
+var compile = GLSL.compile;
+
+var src = `attribute vec3 vertPosition;
+attribute vec2 vertTexCoord;
+attribute vec3 vertNormal;
+
+varying vec2 fragTexCoord;
+varying vec3 fragNormal;
+
+uniform mat4 mWorld = mat4(2);
+uniform mat4 mView;
+uniform mat4 mProj;
+
+void main()
+{
+  fragTexCoord = vertTexCoord;
+  //  fragNormal = (mWorld * vec4(vertNormal, 0.0)).xyz;
+
+  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
+}`
+console.log(compile(src));
